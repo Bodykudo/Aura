9d6b3dc98d3197a59721ea1d401ea23b8c4f2195
Habiba Mohsen <101303283+Habiba-Mohsen@users.noreply.github.com>
2024-02-24 16:45:56 +0200

    habiba edits

---
+++ playground/Filter.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class Filter:
+    def __init__(self, ftype):
+
+        self.filter_type = ftype
+
+    def avg_filter(self, image):
+        pass
+
+    def gaussian_filter(self, image):
+        pass
+
+    def median_filter(self, image):
+        pass
+
