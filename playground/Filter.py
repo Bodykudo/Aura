c00db9fde79ef3c8f4f7ae3235705292a5262a6d
Habiba Mohsen <101303283+Habiba-Mohsen@users.noreply.github.com>
2024-02-27 00:54:24 +0200

    avg filter done bas in grayscale

--- playground/Filter.py
+++ playground/Filter.py
@@ -1,16 +1,37 @@
 import numpy as np
+import cv2
 
 
 class Filter:
     @staticmethod
-    def avg_filter(image):
+    def apply_avg_filter(image,kernel_size):
+
+        # Read Image and convert it to grayscale
+
+        original_image = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+        filtered_image = original_image.copy()
+        rows, cols = original_image.shape
+
+        # Check that Kernel size is ODD
+
+        if kernel_size % 2 == 0:
+            kernel_size += 1
+
+        half_kernel = kernel_size // 2
+
+        for i in range(half_kernel, rows - half_kernel):
+            for j in range(half_kernel, cols - half_kernel):
+                neighborhood = original_image[i - half_kernel:i + half_kernel + 1, j - half_kernel:j + half_kernel + 1]
+                average_value = np.mean(neighborhood)
+                filtered_image[i, j] = int(average_value)
+
+        return filtered_image
+
+    @staticmethod
+    def apply_gaussian_filter(image):
         pass
 
     @staticmethod
-    def gaussian_filter(image):
-        pass
-
-    @staticmethod
-    def median_filter(image):
+    def apply_median_filter(image):
         pass
 
