dc02d94de140d4cc18de058af7a5ccf673bc8259
Bodykudo <bodyalsadek2002@gmail.com>
2024-03-05 00:34:37 +0200

    Update upload.py

--- server/api/endpoints/upload.py
+++ server/api/endpoints/upload.py
@@ -8,22 +8,23 @@ router = APIRouter()
 
 
 @router.post("/api/upload")
-async def uploadImage(file: UploadFile = File(...)):
-    allowedExt = {"png", "jpg", "jpeg"}
-    fileExt = file.filename.split(".")[-1].lower()
-    if fileExt not in allowedExt:
+async def upload_image(file: UploadFile = File(...)):
+    allowed_extensions = {"png", "jpg", "jpeg"}
+    file_extension = file.filename.split(".")[-1].lower()
+    if file_extension not in allowed_extensions:
         raise HTTPException(
             status_code=400, detail="Only PNG, JPG, and JPEG files are allowed."
         )
 
-    imageID = generate_image_id()
-    filePath = os.path.join(uploads_folder, f"{imageID}.{fileExt}")
+    image_id = generate_image_id()
+    filePath = os.path.join(uploads_folder, f"{image_id}.{file_extension}")
     with open(filePath, "wb") as f:
         content = await file.read()
         f.write(content)
+
     return {
         "success": True,
         "filePath": filePath,
-        "fileId": imageID,
+        "fileId": image_id,
         "message": "Image uploaded successfully.",
     }
