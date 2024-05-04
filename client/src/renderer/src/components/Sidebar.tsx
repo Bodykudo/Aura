e6a727ceb0799dada93dacb5b5e1a14fb8c15778
Bodykudo <bodyalsadek2002@gmail.com>
2024-05-04 03:07:57 +0300

    Add new logo and icon

--- client/src/renderer/src/components/Sidebar.tsx
+++ client/src/renderer/src/components/Sidebar.tsx
@@ -1,33 +1,31 @@
 import { routes } from '@renderer/lib/constants';
 import { cn } from '@renderer/lib/utils';
 import { Link } from '@tanstack/react-router';
-import logo from '@renderer/assets/logo2.png';
+import logo from '@renderer/assets/logo.png';
 
 export default function Sidebar() {
   return (
     <div className="flex flex-col space-y-4 py-4 h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
-        <Link to="/" className="flex pl-3 mb-14">
-          <div className="h-32 mx-auto">
+        <Link to="/" className="flex mt-4 mb-8">
+          <div className="h-12 mx-auto">
             <img src={logo} className="mx-auto h-full" alt="logo" />
           </div>
         </Link>
 
         <div className="space-y-1">
-          {routes
-            .filter((route) => !route.comingSoon)
-            .map((route) => (
-              <Link
-                to={route.href}
-                key={route.href}
-                className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 transition-all rounded-lg text-zinc-400"
-              >
-                <div className="flex items-center flex-1">
-                  <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
-                  {route.label}
-                </div>
-              </Link>
-            ))}
+          {routes.map((route) => (
+            <Link
+              to={route.href}
+              key={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 transition-all rounded-lg text-zinc-400"
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
